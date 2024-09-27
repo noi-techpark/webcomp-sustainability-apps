b4c95d3699dbd24255733d6590dbcaa51225b21b
Peter Schweigkofler <peter.schweigkofler@konverto.eu>
2024-09-27 12:00:01 +0200

    fix: include reuse comments in files and add update ignored files in dep5

--- src/assets/locales/i18n.ts
+++ src/assets/locales/i18n.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import { createI18n } from 'vue-i18n';
 import messagesEn from '@/assets/locales/en.json';
 import messagesDe from '@/assets/locales/de.json';
