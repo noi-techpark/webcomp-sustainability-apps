133782bb65004e762780c84654d1325d2530abcb
Manuel Roat <manuel.roat@konverto.eu>
2024-10-25 13:22:02 +0200

    feat: finished preriodica data calling

--- src/api/suedtirolRadelt/suedtirolRadeltApi.ts
+++ src/api/suedtirolRadelt/suedtirolRadeltApi.ts
@@ -16,13 +16,15 @@ const requestedDataTypes = [
 ];
 
 const companyGamificationEndpoint = 'CompanyGamificationAction';
+const origin = 'webcomp-sustainability-apps';
 
 export async function fetchCompaniesAsync(): Promise<Set<string>> {
   const select = ['sname'];
+  const where = 'sactive.eq.true';
   const response = await openDataHubApiClient.get<
     OpenDataHubApiResponse<Company>
   >(
-    `/flat/${companyGamificationEndpoint}/${requestedDataTypes.join(',')}/latest?origin=webcomp-sustainability-apps&where=sactive.eq.true&limit=-1&distinct=true&select=${select.join(',')}`
+    `/flat/${companyGamificationEndpoint}/${requestedDataTypes.join(',')}/latest?origin=${origin}&where=${where}&limit=-1&distinct=true&select=${select.join(',')}`
   );
   return new Set(
     response.data.data.map((item: Company) => item.sname.split('-')[0])
@@ -33,10 +35,11 @@ export async function fetchCompanyGamificationActionsAsync(): Promise<
   SuedtirolRadeltItem[]
 > {
   const select = ['tname', 'ttype', 'tunit', 'mvalue', 'sname', 'stype'];
+  const where = 'sactive.eq.true';
   const response = await openDataHubApiClient.get<
     OpenDataHubApiResponse<SuedtirolRadeltItem>
   >(
-    `/flat/${companyGamificationEndpoint}/${requestedDataTypes.join(',')}/latest?origin=webcomp-sustainability-apps&where=sactive.eq.true&limit=-1&select=${select.join(',')}`
+    `/flat/${companyGamificationEndpoint}/${requestedDataTypes.join(',')}/latest?origin=${origin}&where=${where}&limit=-1&select=${select.join(',')}`
   );
   return response.data.data.map((item: SuedtirolRadeltItem) => ({
     tname: item.tname,
@@ -62,10 +65,15 @@ export async function fetchHistoricDataAsync(
     );
   }
 
+  const select = ['tname', 'ttype', 'tunit', 'mvalue', 'sname', 'stype'];
+  const where = company
+    ? `and(sactive.eq.true,sname.eq.${company})`
+    : 'sactive.eq.true';
+
   const response = await openDataHubApiClient.get<
     OpenDataHubApiResponse<HistoricSuedtirolRadeltItem>
   >(
-    `/flat/${companyGamificationEndpoint}/${requestedDataTypes.join(',')}/${from.toISOString()}/${to.toISOString()}/?origin=webcomp-sustainability-apps&where=sname.eq.${company}&limit=-1`
+    `/flat/${companyGamificationEndpoint}/${requestedDataTypes.join(',')}/${from.toISOString()}/${to.toISOString()}/?origin=${origin}&where=${where}&select=${select}&limit=-1`
   );
   return response.data.data.map((item: HistoricSuedtirolRadeltItem) => ({
     tname: item.tname,
